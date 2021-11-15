9fe5bc7726bec370a1cc767078ae3f11db7cc2aa
thomas.irmer@ibm.com <thomas.irmer@ibm.com>
2021-11-15 13:26:58 +0100

    Re-arrange folder structure and add sample tests

---
+++ app/tests/sample.test.js
@@ -0,0 +1,10 @@
+describe("A sample test", () => {
+    it("used for demonstrative purposes", () => {
+        expect(1 + 2).toEqual(3);
+    });
+
+    // uncomment this test to demonstrate a pipeline failure due to test failure:
+    // it("fails the pipeline if the test is nonsense", () => {
+    //    expect(add(10, 10)).toEqual(42);
+    // });
+})
